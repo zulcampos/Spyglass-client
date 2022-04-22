c1322599467618f63154c98eb053a1e7f7234f86
michellemccool <mefiorella@gmail..com>
2022-04-22 16:22:14 -0400

    changed colors

--- src/pages/GoalForm.js
+++ src/pages/GoalForm.js
@@ -56,7 +56,7 @@ export default function MultilineTextFields() {
       </div>
       <TextField
           style={{background: "rgb(232, 241, 250)"}}
-          id="filled-multiline-static"
+          id="outlined-textarea"
           label="Goal Description"
           placeholder="Enter description here!"
           multiline
